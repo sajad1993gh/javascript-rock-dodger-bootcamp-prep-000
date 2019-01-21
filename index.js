a258775aa5ee635fd082380aa40b0411f26cd8ae
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-21 23:35:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -183,7 +183,13 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-   
+    window.requestAnimationFrame(function() {
+    const left = positionToInteger(DODGER.style.left)
+
+    if (left > 0) {
+      DODGER.style.left = `${left - 4}px`;
+    }
+  })
 }
 
 function moveDodgerRight() {
