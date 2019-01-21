3bbb942df548f35864d510c90527c85c4db04930
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-21 23:34:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -163,10 +163,10 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-   const code = e.which
+   const code = e.which;
 
   if ([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
-    e.preventDefault()
+    e.preventDefault();
     e.stopPropagation()
   }
 
