ed551fabe4431c9eea00065e28eb575f59141ab3
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-21 23:31:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -133,8 +133,6 @@ function createRock(x) {
   // Add the rock to ROCKS so that we can remove all rocks
   ROCKS.push(rock)
   // when there's a collision
-  ROCKS.push(rock)
-
   // Finally, return the rock element you've created
   return rock
 }
